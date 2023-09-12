3e4ef82df3af8354b09dbebfe549fea28f791c9b
Radhika Parasana <89296586+radhika3112@users.noreply.github.com>
2023-09-12 22:34:18 +0530

    Create script.js

---
+++ Project35/script.js
@@ -0,0 +1,9 @@
+let ball1=document.getElementsByClassName("ba1")[0];
+let ball2=document.getElementsByClassName("ba2")[0];
+ball1.addEventListener("mouseover",()=>{
+    ball1.style.background="yellow";
+})
+
+ball2.addEventListener("mouseover",()=>{
+    ball2.style.background="pink";
+})
