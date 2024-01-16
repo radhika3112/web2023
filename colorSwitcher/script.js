cf3e4fcb44a5dbb40f569a31aa7680e6eb3f7b51
Radhika Parasana <89296586+radhika3112@users.noreply.github.com>
2024-01-16 13:35:12 +0530

    Create script.js

---
+++ colorSwitcher/script.js
@@ -0,0 +1,14 @@
+let b1=document.getElementsByClassName("btn1")[0];
+b1.addEventListener("click",()=>{
+    document.querySelector("body").style.backgroundColor="palevioletred"
+})
+
+let b2=document.getElementsByClassName("btn2")[0];
+b2.addEventListener("click",()=>{
+    document.querySelector("body").style.backgroundColor="blueviolet"
+})
+
+let b3=document.getElementsByClassName("btn3")[0];
+b3.addEventListener("click",()=>{
+    document.querySelector("body").style.backgroundColor="yellowgreen"
+})
